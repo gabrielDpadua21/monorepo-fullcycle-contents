ad7651869361eb9bc9b25614914ed2819c0e7f49
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-08-08 10:56:34 -0300

    ✨feat: grpc exemples

---
+++ exemples/grpc/cmd/server/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/codeedu/fc2-grpc/pb"
+	"github.com/codeedu/fc2-grpc/services"
+	"google.golang.org/grpc"
+	//"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCatServiceServer(grpcServer, &services.CatService{})
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatal("Could not serve: %v", err)
+	}
+}
---
+++ exemples/grpc/services/cat.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	//"io"
+	//"log"
+	//"time"
+
+	"github.com/codeedu/fc2-grpc/pb"
+)
+
+type CatService struct {
+	pb.UnimplementedCatServiceServer
+}
+
+func NewCatService() *CatService {
+	return &CatService{}
+}
+
+func (*CatService) AddCat(ctx context.Context, request *pb.Cat) (*pb.Cat, error) {
+
+	fmt.Println(request.Name)
+	id := "123"
+
+	return &pb.Cat{
+		Id: &id,
+		Name: request.GetName(),
+		Color: request.GetColor(),
+		Age: request.GetAge(),
+	}, nil
+
+}
